2d13bd5cfa3ad0b6c85b48a9d7ee3c0cb0ca8e17
Jhoan Sebastian Colorado Vargas <jhoansebastian1009@gmail.com>
2026-09-05 16:15:27 -0500

    Modulo 5

--- reserva-restaurante/src/Main.kt
+++ reserva-restaurante/src/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
+    val tableManager = TableManager()
+    val orderManager = OrderManager(tableManager)
     var opcion: Int
-    val reservationSystem = ReservationSystem()
 
     println(" BIENVENIDO AL SISTEMA DE RESERVAS DE RESTAURANTE ")
 
@@ -9,11 +10,17 @@ fun main() {
         opcion = UIController.leerEntero("Ingrese la opción deseada (0-6): ")
 
         when (opcion) {
-            1 -> println(" 1. --- Módulo de Mesas")
-            2 -> println(" 2. --- Módulo de Clientes")
-            3 -> reservationSystem.gestionarReservas() // <-- Conexión del módulo de reservas
-            4 -> println(" 4. --- Catálogo de Menú")
-            5 -> println(" 5. --- Módulo de Pedidos")
+            1 -> tableManager.gestionarMesas()
+            2 -> {
+                println(" 2. --- Módulo de Clientes")
+                UIController.gestionarClientes()
+            }
+            3 -> println(" 3. --- Módulo de Reservas")
+            4 -> {
+                println(" 4. --- Catálogo de Menú")
+                MenuCatalog.mostrarCatalogo()
+            }
+            5 -> orderManager.gestionarPedidos()
             6 -> println(" 6. --- Módulo de Facturación")
             0 -> println(" Saliendo del sistema... ¡Gracias por usar la aplicación!")
             else -> println(" Opción no válida. Por favor ingrese un número entre 0 y 6.")
